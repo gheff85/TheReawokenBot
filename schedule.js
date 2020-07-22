861ae6526c432fe25f454dea44e6a13b0b52efbb
gheff85 <67950967+gheff85@users.noreply.github.com>
2020-07-22 09:07:51 +0100

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,14 +1,1 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
 
-client.on('message', message => {
-  if (message.content.toLowerCase() === '!rb startSchedule') {
-    var channel = message.channel;
-    channel.send("Schedule Started");
-    setInterval(function(){
-        channel.send("Test scheduled message");
-    }, 30000);
-  }
-});
-
-client.login(process.env.BOT_TOKEN);
