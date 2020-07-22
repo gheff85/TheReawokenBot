01290e1f901602c6a8698dd38478bace4d1792aa
gheff85 <67950967+gheff85@users.noreply.github.com>
2020-07-22 08:08:28 +0100

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,6 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', () => {
+
+var guild = client.guild.get(process.env.GUILD_ID);
+var unRegChannel = guild.channels.get(process.env.UNREGMEMBERS);
+
 setInterval(function(){
 
+unRegChannel.sendMessage("Test Message");
 }, 20000);
+});
+
+client.login(process.env.BOT_TOKEN);
