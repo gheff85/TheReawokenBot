15b782bb1f8c356388bb0e8b1005779cebd58ecd
gheff85 <67950967+gheff85@users.noreply.github.com>
2020-07-24 17:54:57 +0100

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,16 +1,1 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
 
-client.on('ready', () => {
-    var inactiveUsers = client.channels.get(process.env.UNREGMEMBERS);
-    inactiveUsers.send("Searching for inactive users");
-    setInterval(function(){
-       message.channel.send('Test message');
-   }, 30000);
-
-  }
-});
-
-
-
-client.login(process.env.BOT_TOKEN);
