9bcfd35a4b6bf92dbd55cc330f5ff98c6c3332b4
gheff85 <67950967+gheff85@users.noreply.github.com>
2020-07-25 08:09:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    var inactiveUsers = client.channels.get(process.env.UNREGMEMBERS).catch(e => {
-     console.error(e)
-    });
-    inactiveUsers.send("Searching for inactive users");
-    setInterval(function(){
-       message.channel.send('Test message').catch(e => {
-       console.error(e)
-     });
-   }, 30000);
-
-  });
-
 
 client.on('message', message => {
   if (message.content.toLowerCase() === '!rb test') {
