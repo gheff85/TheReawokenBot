fbac141e913af4109f3268179e49676f819c7279
gheff85 <67950967+gheff85@users.noreply.github.com>
2020-12-01 20:53:09 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 var pg = require('pg');
-var conString = process.env.DATABASE_URL+"?ssl=true";
+var conString = process.env.DATABASE_URL;
 
 
 
