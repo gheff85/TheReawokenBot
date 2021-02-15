e102dd019b25d8328057f2314aad9dd27c58691b
gheff85 <67950967+gheff85@users.noreply.github.com>
2021-02-15 22:07:54 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,6 +21,8 @@ client.on("message", async(msg) => {
   }
   
   /////////////////////////////assign role on register////////
+  console.log(msg.channel.id);
+  console.log(msg.content);
   if(msg.channel.id === process.env.REGISTER_HERE_CHANNEL && msg.content.toLowerCase().includes("Successfully synced")){
     console.log("Member has been synced");
     };
