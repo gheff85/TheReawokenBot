830aad50344d2f5fc45b738090e10e0a370df95f
= <g@h.com>
2021-04-11 15:53:21 +0100

    adding new bot with command handler

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-worker: node bot.js
-Scheduled: node schedule.js
+worker: node ReawokenBot/src/bot.js
---
+++ ReawokenBot/src/events/message/MessageEvent.js
@@ -0,0 +1,41 @@
+const BaseEvent = require('../../utils/structures/BaseEvent');
+const common = require("../../utils/common/commonFunctions");
+module.exports = class MessageEvent extends BaseEvent {
+  constructor() {
+    super('message');
+  }
+  
+  async run(client, message) {
+    if (message.author.bot) return;
+
+    let messageContent;
+
+    if(message.content
+      .toLowerCase() === "!register"){
+        messageContent = "register"
+    }
+    else {
+      messageContent = message.content
+      .toLowerCase()
+      .slice(client.prefix.length)
+      .trim();
+    }
+
+    await common.logLastUsersMessageTimestamp(message.author.id, new Date())
+
+    if(messageContent !== "rankcard"){
+      await common.generateExperience(message)
+    }
+
+    if (message.content.startsWith(client.prefix) || messageContent === "register") {
+      const [cmdName, ...cmdArgs] = messageContent
+                                    .split(/\s+/);
+
+      const command = client.commands.get(cmdName);
+      if (command) {
+       await command.run(client, message, cmdArgs)
+            .catch((e) => message.channel.send(e.message));
+      }
+    }
+  }
+}
