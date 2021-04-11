ba79c5a2d24cf5a3750779e7b5a62fe345071e4c
= <g@h.com>
2021-04-11 16:26:52 +0100

    update reaction code

---
+++ ReawokenBot/src/events/messageReactionRemove/MessageReactionRemoveEvent.js
@@ -0,0 +1,13 @@
+const BaseEvent = require('../../utils/structures/BaseEvent');
+const common = require("../../utils/common/commonFunctions");
+
+module.exports = class MessageReactionRemoveEvent extends BaseEvent {
+  constructor() {
+    super('messageReactionRemove');
+  }
+  async run (messageReaction, member) {
+    if (member.bot) return;
+
+    await common.logLastUsersMessageTimestamp(member.id, new Date())
+  }
+}
